05d86271346fc0d14a891670ea12128d619f1f9b
ARK <39636224+AlirezaKamrani@users.noreply.github.com>
2023-10-07 00:30:40 +0330

    Initializing the project

---
+++ src/main/java/com/ark/reactivemp4analyser/ReactiveMp4AnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.ark.reactivemp4analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMp4AnalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMp4AnalyserApplication.class, args);
+    }
+
+}
